233b358c1d967b8060deda512b46ca7b9fa876ec
redaphid <iam@aaronherres.com>
2019-10-02 14:16:44 -0700

    silly exercise for silly people

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What number do you like?");
+    println!("Hint: it's 17");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+       .expect("Your terminal is fucked. You should do something about that.");
+    println!("You like {}, eh? Interesting. Wrong, but interesting.", guess);
 }
